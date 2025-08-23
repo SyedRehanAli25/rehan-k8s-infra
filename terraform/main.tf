6ca4bb03ed3ca08ab74490c1a61d55d91341dd2e
Syed Rehan Ali <rehan.ali9325@gmail.com>
2025-08-23 17:40:01 +0530

    Update main.tf to use relative path for SSH public key

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,6 +1,6 @@
 resource "aws_key_pair" "k8s_key" {
   key_name   = var.key_name
-  public_key = file("/home/rehan/.ssh/id_rsa.pub")
+  public_key = file("${path.module}/id_rsa.pub")
 }
 
 resource "aws_security_group" "k8s_sg" {
